649788610a5d09d6cf428265f8105b9b024d53bf
Matthew <pietraszewski.mateusz@gmail.com>
2015-12-11 13:08:15 +0000

    Transform problem

--- Assignment1/Assets/CustomCyllinder.cs
+++ Assignment1/Assets/CustomCyllinder.cs
@@ -14,35 +14,41 @@ public class CustomCyllinder : MonoBehaviour {
     void Start()
     {
         circles = new List<Circle>();
-        Circle newCircle = CreateCircleAt(transform, radius, numberOfPoints);
+        Circle newCircle = new Circle(CreateCircleAt(transform, radius, numberOfPoints));
         circles.Add(newCircle);
-
+        transform.Translate(Vector3.forward * (radius * 2));
+        Circle anotherCircle = new Circle(CreateCircleAt(transform, radius, numberOfPoints));
+        circles.Add(anotherCircle);
     }
 
-    Circle CreateCircleAt(Transform _centre, float _radius, int _numpoints)
+    List<Vector3> CreateCircleAt(Transform _centre, float _radius, int _numpoints)
     {
+        Debug.Log("Current Position: "+_centre.position);
+        Vector3 above = (_centre.up * _radius);
+        Debug.Log("Point Above Me: "+ above);
         List<Vector3> newPoints = new List<Vector3>();
         float theta = Mathf.PI * 2.0f / _numpoints;
+        Quaternion temp = _centre.rotation;
         for (int i = 0; i < _numpoints; i++)
         {
             _centre.Rotate(Vector3.forward * (theta * Mathf.Rad2Deg));
             Vector3 newPoint = (_centre.up * _radius);
             newPoints.Add(newPoint);
         }
-
-        return new Circle(newPoints);
+        _centre.rotation = temp;
+        return newPoints;
     }
 
     void OnDrawGizmos()
     {
         if (circles != null)
         {
-            foreach (Circle c in circles)
+            for (int i = 0; i < circles.Count; i++)
             {
-                for (int j = 1; j <= c.circlePoints.Count; j++)
+                for (int j = 1; j <= circles[i].circlePoints.Count; j++)
                 {
-                    Vector3 prev = c.circlePoints[j - 1];
-                    Vector3 next = c.circlePoints[j % c.circlePoints.Count];
+                    Vector3 prev = circles[i].circlePoints[j - 1];
+                    Vector3 next = circles[i].circlePoints[j % circles[i].circlePoints.Count];
                     Gizmos.color = Color.green;
                     Gizmos.DrawLine(prev, next);
                 }
